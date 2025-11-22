04cfb474abbfd50cc681020ef7f5885fe5c15ce3
del3500 <wendellsmesia@gmail.com>
2025-11-23 01:51:29 +0800

    initial commit

---
+++ backend/app/api/server.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+def get_application():
+    app = FastAPI(title="Securix", version="1.0.0")
+    
+    # Browser blocks cross-domain API calls by default
+    app.add_middleware(
+        CORSMiddleware,
+        # This allows any website to make request (development use only)
+        # in prod, I have to change this to "allow_origins=["https://myfrontend.com"]"
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
+    return app
+
+app = get_application()
