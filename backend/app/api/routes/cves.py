0bddbda0282438b4848a7d7e19c0cee0ebe1f813
del3500 <wendellsmesia@gmail.com>
2026-01-02 03:00:29 +0800

    bug fixes

--- backend/app/api/routes/cves.py
+++ backend/app/api/routes/cves.py
@@ -1,13 +1,18 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Path, HTTPException
 from app.api.dependencies.database import get_repository
 from app.models.cve import CVEDetail
 from app.db.repositories.cves import CVERepository
 
+from starlette.status import HTTP_404_NOT_FOUND
+
 router = APIRouter()
 
-
-@router.get("/", response_model=list[CVEDetail], name="cves:get-all-cves")
-async def get_all_cves(
-    cves_repo: CVERepository = Depends(get_repository(CVERepository)),
-) -> list[CVEDetail]:
-    return await cves_repo.get_all_cves()
+@router.get("/{cve_id_query}", response_model=CVEDetail, name="cves:get_cve_by_id")
+async def get_cve_by_id(
+    cve_id_query: str = Path(required=True, title="CVE to get"),
+    cve_repo = CVERepository = Depends(get_repository(CVERepository))
+) -> CVEDetail:
+    cve = await cve_repo.get_cve_by_id(cve_id_query=cve_id_query)
+    if not cve:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="No cve id found")
+    return cve
--- backend/app/db/repositories/cves.py
+++ backend/app/db/repositories/cves.py
@@ -1,16 +1,41 @@
 from app.db.repositories.base import BaseRepository
 from app.models.cve import CVEDetail
 
-
 class CVERepository(BaseRepository):
     """
     All database actions associated with the CVE resources
     """
 
-    async def get_all_cves(self) -> list[CVEDetail]:
-        GET_ALL_CVES_QUERY = """
-            SELECT cve_id, source_identifiers, published_at, last_modified_at, vuln_status, descriptions, cvss_metrics
-            FROM cves;
+    async def get_cve_by_id(self, *, cve_id: str) -> CVEDetail:
+        query="""
+        SELECT
+            c.id,
+            c.cve_id,
+            c.published_at,
+            c.last_modified_at,
+        
+            COALESCE(d.descriptions, '[]'::json) AS descriptions
+            COALESCE(m.cvss_metrics, '[]'::json) AS cvss_metrics
+
+        FROM cves c
+
+        LEFT JOIN LATERAL (
+            SELECT json_agg(d ORDER BY d.id) AS descriptions
+            FROM cve_descriptions d
+            WHERE d.cve_id = c.id
+        ) d ON TRUE
+
+        LEFT JOIN LATERAL (
+            SELECT json_agg(m ORDER BY m.id) AS cvss_metrics
+            FROM cvss_metrics m
+            WHERE m.cve_id = c.id
+        ) m ON TRUE
+
+        WHERE c.cve_id = :cve_id
         """
-        cve_records = await self.db.fetch_all(query=GET_ALL_CVES_QUERY)
-        return [CVEDetail(**cve) for cve in cve_records]
+
+        cve = await self.db.fetch_one(query=query, values={"cve_id": cve_id})
+        if not cve:
+            return None
+        return CVEDetail(**cve)
+
