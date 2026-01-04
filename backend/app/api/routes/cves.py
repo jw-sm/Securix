ba2562c9e5d9ce5608a98fa70b7f8f11e6d64bda
del3500 <wendellsmesia@gmail.com>
2026-01-05 06:48:45 +0800

    updated imports

--- backend/app/api/routes/cves.py
+++ backend/app/api/routes/cves.py
@@ -1,17 +1,18 @@
 from fastapi import APIRouter, Depends, Path, HTTPException
 from app.api.dependencies.database import get_repository
-from app.models.cve import CVEDetail
 from app.db.repositories.cves import CVERepository
+from app.models.cve import CVE
 
 from starlette.status import HTTP_404_NOT_FOUND
 
 router = APIRouter()
 
-@router.get("/{cve_id_query}", response_model=CVEDetail, name="cves:get_cve_by_id")
+
+@router.get("/{cve_id_query}", response_model=CVE, name="cves:get_cve_by_id")
 async def get_cve_by_id(
     cve_id_query: str = Path(required=True, title="CVE to get"),
     cve_repo: CVERepository = Depends(get_repository(CVERepository))
-) -> CVEDetail:
+) -> CVE:
     cve = await cve_repo.get_cve_by_id(cve_id=cve_id_query)
     if not cve:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="No cve id found")
